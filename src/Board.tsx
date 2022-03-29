5ccf8f0f9c024f30a0c5c077a99463e3ce769a49
Patainea Robert <patainearobert@yahoo.com>
2022-03-29 18:31:17 +0300

    generated random numbers

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,21 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Board.css";
 import Cell from "./Cell";
 
 type Props = {};
 
+//const cellIdx = Math.floor(Math.random() * 16);
+
 export default function Board({}: Props) {
-    const displayBoard = () => {
+    const [board, setBoard] = useState([
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+    ]);
 
-        const getValue = (cellIdx: number) => {
-            if (cellIdx == 6) return "2";
-            if (cellIdx == 9) return "4";
-            return "";
+    useEffect(() => {
+        let array = [...board];
+        const insertRandomCell = () => {
+            let cellIdx = 0;
+            do {
+                cellIdx = Math.floor(Math.random() * 16);
+            } while (array[cellIdx] !== "");
+            const randomNumber = Math.floor(Math.random() * 10);
+            if (randomNumber >= 5) array[cellIdx] = "4";
+            else array[cellIdx] = "2";
+
         }
+        insertRandomCell();
+        insertRandomCell();
+        setBoard(array);
+    }, []);
 
+    const displayBoard = () => {
         const cells = [];
         for (let i = 0; i < 16; i++) {
-            cells.push(<Cell value={getValue(i)}/>);
+            cells.push(<Cell value={board[i]} />);
         }
         return cells;
     };
